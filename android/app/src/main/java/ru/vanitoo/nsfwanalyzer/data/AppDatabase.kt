83f9075b97ab4d7e8b48380e7962c2d7d6cc237f
vanitoo <56897888+vanitoo@users.noreply.github.com>
2026-09-25 03:09:18 +0300

    android: add standalone app scaffold

---
+++ android/app/src/main/java/ru/vanitoo/nsfwanalyzer/data/AppDatabase.kt
@@ -0,0 +1,13 @@
+package ru.vanitoo.nsfwanalyzer.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [ImageAnalysisEntity::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun imageAnalysisDao(): ImageAnalysisDao
+}
